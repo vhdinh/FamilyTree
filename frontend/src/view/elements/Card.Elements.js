40ce2655b88da323b4b2dfc6da3b771711be4a0d
Vu Dinh <isthis.vu@gmail.com>
2024-04-15 12:51:09 -0700

    fix: update image

--- frontend/src/view/elements/Card.Elements.js
+++ frontend/src/view/elements/Card.Elements.js
@@ -150,7 +150,7 @@ export function CardImage({d, image, card_dim, maleIcon, femaleIcon}) {
   return ({template: (`
     <g style="transform: translate(${card_dim.img_x}px,${card_dim.img_y}px);" class="card_image" clip-path="url(#card_image_clip)">
       ${image 
-        ? `<image href="${image}" height="${card_dim.img_h}" width="${card_dim.img_w}" preserveAspectRatio="xMidYMin slice" />`
+        ? `<image href="${image}" height="${card_dim.img_h}" width="${card_dim.img_w}" preserveAspectRatio="xMidYMin slice" referrerpolicy="no-referrer" />`
         : (d.data.data.gender === "F" && !!femaleIcon) ? femaleIcon({card_dim}) 
         : (d.data.data.gender === "M" && !!maleIcon) ? maleIcon({card_dim}) 
         : GenderlessIcon()
